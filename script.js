ec5b2958f6cb371a51f45f7ee7853f5347096645
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2024-01-09 14:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function Onetwenty(n) {
+	for (let i = 1; i <=n; i++) {
+		if(i%3==0 && i%5==0)
+		{
+			alert("FizzBuzz");
+		}
+	 else if(i%3==0)
+		{
+           alert("Fizz");
+		}
+		else if(i%5==0)
+		{
+			alert("Buzz");
+		}
+		else{
+			alert(i);
+		}
+		
+		
+	}
+}
+
+	
+
+Onetwenty(20)
